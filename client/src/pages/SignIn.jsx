974642648f66454a801c9e47d447d23afeeb3e3f
rohan <panditrohan738@gmail.com>
2024-06-30 09:58:13 +0530

    auth in sign up page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -15,11 +15,7 @@ export default function SignUp(){
     setFormData({
       ...formData, [e.target.id]: e.target.value,
     })
-    
-  
-    
-
-  }
+}
 
   const handleSubmit = async (e) => {
     e.preventDefault();
